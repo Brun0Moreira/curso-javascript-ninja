d3a6f34bfa20167d801d0efec37f072e441114e0
Brun0Moreira <brunomacedomoreira@hotmail.com>
2020-09-30 11:04:04 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -95,7 +95,6 @@ Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-var bookSelected = book(nameBook);
 console.log('O autor do livro ' + nameBook + ' é ' + bookSelected.autor + '.');
 
 /*
@@ -103,5 +102,4 @@ Ainda com a função acima, imprima o nome da editora de um livro qualquer, usan
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-var bookSelected = book(nameBook);
 console.log('O livro ' + nameBook + ' foi publicado pela editora ' + bookSelected.editora + '.');
